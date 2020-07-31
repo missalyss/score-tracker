ff48bf22bd21a2b34b396987b7c9acb2ef7a0529
Alyssa Helgason <alyssahelgason@Alyssas-MacBook-Pro.local>
2020-07-30 18:30:16 -0700

    it sorrrrrt of works with the score, the ui is just super messed up

--- src/components/player.js
+++ src/components/player.js
@@ -1,26 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles/player.css';
 import Score from './score';
 
-const Player = ({ name, setPlayerName, number }) => {
-  const [score, setScore] = useState('LOU');
+const Player = ({ name, setPlayerName, playerNumber }) => {
+  const [score, setScore] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [roundNumber, setRoundNumber] = useState(0);
+  const [scoreList] = useState([]);
+
+  useEffect(() => {
+    scoreList.push({ roundNumber, score });
+    console.log('===SCORELIST===: ', scoreList);
+  }, [roundNumber]);
+
 
   return (
     <div className="">
-
       <div className="playerWrap">
         <h2 className="text-neon-green p0 m0">
-          Player {number}:&nbsp;
+          Player {playerNumber}:&nbsp;
         <input
             className="playerName"
             type="text"
             onChange={(e) => setPlayerName(e.target.value)}
-            value={`${name}`}
+            value={name}
             maxLength="10"
           />
         </h2>
       </div>
-      <Score score={score} setScore={setScore} />
+      <div>
+        <Score key='scoreone' score={score} setScore={setScore} setTotal={setTotal} total={total} roundNumber={roundNumber} setRoundNumber={setRoundNumber} />
+        {scoreList.map((scoreAndRound, i) => {
+          const { roundNumber: currentRoundNumber, score: currentScore } = scoreAndRound;
+          console.log('score and round: ', scoreAndRound);
+          return (<Score key={i} score={currentScore} setScore={setScore} setTotal={setTotal} total={total} roundNumber={currentRoundNumber} setRoundNumber={setRoundNumber} />)
+        })}
+      </div>
+      <p>Total: {total}</p>
     </div>
   );
 }
--- src/components/score.js
+++ src/components/score.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './styles/player.css';
 
-const Score = ({ score, setScore, number }) => {
+const Score = ({ score, setScore, setTotal, total, setRoundNumber, roundNumber }) => {
+
   return (
     <div className="">
-      round 1:&nbsp;
+      round {roundNumber}:&nbsp;
       <input
-        className="playerName"
+        className="playerName pb5"
         type="number"
-        onChange={(e) => setScore(e.target.value)}
+        onChange={(e) => {
+          const newScore = e.target.valueAsNumber || 0
+          console.log('etarval: ', e.target.valueAsNumber);
+          setScore(newScore)
+        }}
+        onBlur={() => {
+          setTotal(score + total);
+          setRoundNumber(roundNumber + 1);
+          // setScore(0)
+        }}
         placeholder="0"
+        value={score}
         maxLength="10"
       />
     </div>
