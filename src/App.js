bfdc779a84629b0e2edfadd696602a6e6be419dd
aevans <aevans@starbucks.com>
2022-12-21 10:03:43 -0800

    updated pattern library to include more styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import './pattern-lib/styles-list.css';
+import './pattern-lib/flex.css';
+import './pattern-lib/misc.css';
+import './pattern-lib/font.css';
+import './pattern-lib/color.css';
 import './pattern-lib/padding-margin.css';
-import Root from './components';
+import HomePage from './components';
 
 const App = () =>
-  <div className="App">
-    <Root />
-  </div>
+  <main className="bg-black width-100 height-100">
+    <HomePage />
+  </main>
 
 export default App;
